4e863c2aacfb96d380534510e691ac0d2209bf48
ballas01 <rus.bayramov.05@inbox.ru>
2025-04-29 19:49:40 +0400

    Console app basic unit testing

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp.Utils;
+
+Console.WriteLine("Hello, World!");
+
+Operation operation = new();
+int res = operation.SumArr([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+Console.WriteLine(res);
---
+++ ConsoleApp/Utils/Operation.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.Utils;
+
+public class Operation
+{
+    public int SumArr(int[] nums)
+    {
+        if (nums.Length > 10)
+            throw new Exception($"The length is greater than 10, its {nums.Length}");
+
+        int sum = 0;
+        foreach (int i in nums)
+            sum += i;
+
+        return sum;
+    }
+}
